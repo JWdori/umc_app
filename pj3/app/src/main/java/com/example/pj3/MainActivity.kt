34e362b35f0575d2d0e52275f24df83d7eae11a9
Dori <jaewan0114@naver.com>
2022-10-11 18:03:24 +0900

    미션1clear

--- pj3/app/src/main/java/com/example/pj3/MainActivity.kt
+++ pj3/app/src/main/java/com/example/pj3/MainActivity.kt
@@ -4,15 +4,27 @@ import android.app.appsearch.AppSearchManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.pj3.databinding.ActivityFirstBinding
 import com.example.pj3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-             supportFragmentManager.beginTransaction().replace(viewBinding.framFragment.id,FirstFragment()).commitAllowingStateLoss()
+
+        viewBinding.button.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("text",viewBinding.editbtn.text.toString())
+
+            startActivity(intent)
+        }
+
+        supportFragmentManager.beginTransaction().replace(viewBinding.framFragment.id,FirstFragment()).commitAllowingStateLoss()
 
         viewBinding.btnFragment1.setOnClickListener{
             supportFragmentManager

76bf3107121608805ba0b246a588dece89d88c84
Dori <jaewan0114@naver.com>
2022-11-25 12:49:57 +0900

    8주차 과제 이어 해야지...

---
+++ hw8_2/app/src/main/java/com/example/hw8_2/SecondActivity.kt
@@ -0,0 +1,36 @@
+package com.example.hw8_2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hw8_2.databinding.ActivitySecondBinding
+
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val dataList: ArrayList<data> = arrayListOf()
+        dataList.apply {
+            add(data("gd", "dd"))
+            add(data("gd", "dd"))
+            add(data("어쩔", "dd"))
+
+        }
+        binding=ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        binding.txtSecond.text=intent.getStringExtra("data")
+        binding.btn2.setOnClickListener{
+            intent.putExtra("data",binding.txtSecond.text.toString())
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+
+
+    }
+}
